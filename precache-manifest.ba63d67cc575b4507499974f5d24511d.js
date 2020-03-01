a091b314f3654e877e78fd7390e3611e84baa275
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-03-01 19:20:43 +0500

    Updates

---
+++ precache-manifest.ba63d67cc575b4507499974f5d24511d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6f1b2ced8511369f25c54cfc22db8c",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "cdd62d351e518b4ae61d",
+    "url": "/React-Todo-App/static/css/main.3ea8f413.chunk.css"
+  },
+  {
+    "revision": "2fee86cadefcd4c74710",
+    "url": "/React-Todo-App/static/js/2.44254d61.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Todo-App/static/js/2.44254d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd62d351e518b4ae61d",
+    "url": "/React-Todo-App/static/js/main.0ee8f55e.chunk.js"
+  },
+  {
+    "revision": "923ec40e0d51edb21874",
+    "url": "/React-Todo-App/static/js/runtime-main.37fd90b3.js"
+  }
+]);
